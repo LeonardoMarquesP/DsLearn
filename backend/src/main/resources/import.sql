ba758fa0b34f0db557ef5d4ff77ead0ed5804f89
LeonardoMarquesP <leonardom.pereira@hotmail.com>
2024-10-27 22:42:34 -0300

    Notification, Offer, and Course Entities

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -13,3 +13,10 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_notification (text, moment, read, route, user_id) VALUES ('Notif 1', TIMESTAMP WITH TIME ZONE '2024-10-27T10:00:00.12345Z', 'false', 'Rota1', 2);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'URLImagem1', 'URLImagem1Cinza');
+
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2024-11-20T03:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2025-11-20T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2024-12-20T03:00:00.12345Z', TIMESTAMP WITH TIME ZONE '2025-12-20T03:00:00Z', 1);
